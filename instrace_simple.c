0d9fbdfde3c142f53640c86ba104e486cadbab43
Toshi Piazza <toshi.piazza@gmail.com>
2017-08-19 17:00:57 -0400

    Adds instrace_simple sample converted to drx_buf

--- instrace_simple.c
+++ instrace_simple.c
@@ -36,25 +36,21 @@
 #include "dr_api.h"
 #include "drmgr.h"
 #include "drreg.h"
-#include "drutil.h"
 #include "drx.h"
 #include "utils.h"
 
-enum {
-    REF_TYPE_READ  = 0,
-    REF_TYPE_WRITE = 1,
-};
-typedef struct _mem_ref_t {
-    ushort type;
-    ushort size;
-    app_pc addr;
-} mem_ref_t;
+/* Each ins_ref_t describes an executed instruction. */
+typedef struct _ins_ref_t {
+    app_pc pc;
+    int opcode;
+} ins_ref_t;
 
-#define MAX_NUM_MEM_REFS 4096
-#define MEM_BUF_SIZE (sizeof(mem_ref_t) * MAX_NUM_MEM_REFS)
-
-static drx_buf_t *trace_buffer;
-static int tls_idx;
+/* Max number of ins_ref a buffer can have. It should be big enough
+ * to hold all entries between clean calls.
+ */
+#define MAX_NUM_INS_REFS 8192
+/* The maximum size of buffer for holding ins_refs. */
+#define MEM_BUF_SIZE (sizeof(ins_ref_t) * MAX_NUM_INS_REFS)
 
 /* thread private log file and counter */
 typedef struct {
@@ -62,46 +58,40 @@ typedef struct {
     FILE      *logf;
 } per_thread_t;
 
+static drx_buf_t *trace_buffer;
+static int      tls_idx;
+
 static client_id_t client_id;
 
 static void
-memtrace(void *drcontext, void *buf_base, size_t size)
+instrace(void *drcontext, void *buf_base, size_t size)
 {
     per_thread_t *data;
-    mem_ref_t *mem_ref, *buf_ptr;
+    ins_ref_t *ins_ref, *buf_ptr;
 
-    data = drmgr_get_tls_field(drcontext, tls_idx);
-    buf_ptr = (mem_ref_t *)((byte *)buf_base + size);
-    /* Example of dumpped file content:
-     *   0x00007f59c2d002d3:  5, call
-     *   0x00007ffeacab0ec8:  8, w
+    data    = drmgr_get_tls_field(drcontext, tls_idx);
+    buf_ptr = (ins_ref_t *)((byte *)buf_base + size);
+    /* Example of dumped file content:
+     *   0x7f59c2d002d3: call
+     *   0x7ffeacab0ec8: mov
      */
     /* We use libc's fprintf as it is buffered and much faster than dr_fprintf
      * for repeated printing that dominates performance, as the printing does here.
      */
-    for (mem_ref = (mem_ref_t *)buf_base; mem_ref < buf_ptr; mem_ref++) {
+    for (ins_ref = (ins_ref_t *)buf_base; ins_ref < buf_ptr; ins_ref++) {
         /* We use PIFX to avoid leading zeroes and shrink the resulting file. */
-        fprintf(data->logf, ""PIFX": %2d, %s\n", (ptr_uint_t)mem_ref->addr, mem_ref->size,
-                (mem_ref->type > REF_TYPE_WRITE) ?
-                decode_opcode_name(mem_ref->type) /* opcode for instr */ :
-                (mem_ref->type == REF_TYPE_WRITE ? "w" : "r"));
+        fprintf(data->logf, PIFX",%s\n", (ptr_uint_t)ins_ref->pc,
+                decode_opcode_name(ins_ref->opcode));
     }
 }
 
 static void
-insert_save_type(void *drcontext, instrlist_t *ilist, instr_t *where,
-                 reg_id_t base, reg_id_t scratch, ushort type)
+insert_save_opcode(void *drcontext, instrlist_t *ilist, instr_t *where,
+                   reg_id_t base, reg_id_t scratch, int opcode)
 {
     drx_buf_insert_buf_store(drcontext, trace_buffer, ilist, where, base, scratch,
-                             OPND_CREATE_INT16(type), OPSZ_2, offsetof(mem_ref_t, type));
-}
-
-static void
-insert_save_size(void *drcontext, instrlist_t *ilist, instr_t *where,
-                 reg_id_t base, reg_id_t scratch, ushort size)
-{
-    drx_buf_insert_buf_store(drcontext, trace_buffer, ilist, where, base, scratch,
-                             OPND_CREATE_INT16(size), OPSZ_2, offsetof(mem_ref_t, size));
+                             OPND_CREATE_INT16(opcode), OPSZ_2,
+                             offsetof(ins_ref_t, opcode));
 }
 
 static void
@@ -110,22 +100,7 @@ insert_save_pc(void *drcontext, instrlist_t *ilist, instr_t *where,
 {
     drx_buf_insert_buf_store(drcontext, trace_buffer, ilist, where, base, scratch,
                              OPND_CREATE_INTPTR(pc), OPSZ_PTR,
-                             offsetof(mem_ref_t, addr));
-}
-
-static void
-insert_save_addr(void *drcontext, instrlist_t *ilist, instr_t *where,
-                 opnd_t ref, reg_id_t reg_ptr, reg_id_t reg_addr)
-{
-    bool ok;
-    /* we use reg_ptr as scratch to get addr */
-    ok = drutil_insert_get_mem_addr(drcontext, ilist, where, ref, reg_addr, reg_ptr);
-    DR_ASSERT(ok);
-
-    drx_buf_insert_load_buf_ptr(drcontext, trace_buffer, ilist, where, reg_ptr);
-    drx_buf_insert_buf_store(drcontext, trace_buffer, ilist, where, reg_ptr, DR_REG_NULL,
-                             opnd_create_reg(reg_addr), OPSZ_PTR,
-                             offsetof(mem_ref_t, addr));
+                             offsetof(ins_ref_t, pc));
 }
 
 /* insert inline code to add an instruction entry into the buffer */
@@ -141,96 +116,36 @@ instrument_instr(void *drcontext, instrlist_t *ilist, instr_t *where)
         DR_ASSERT(false); /* cannot recover */
         return;
     }
+
     drx_buf_insert_load_buf_ptr(drcontext, trace_buffer, ilist, where, reg_ptr);
-    insert_save_type(drcontext, ilist, where, reg_ptr, reg_tmp,
-                     (ushort)instr_get_opcode(where));
-    insert_save_size(drcontext, ilist, where, reg_ptr, reg_tmp,
-                     (ushort)instr_length(drcontext, where));
     insert_save_pc(drcontext, ilist, where, reg_ptr, reg_tmp,
                    instr_get_app_pc(where));
+    insert_save_opcode(drcontext, ilist, where, reg_ptr, reg_tmp,
+                       instr_get_opcode(where));
     drx_buf_insert_update_buf_ptr(drcontext, trace_buffer, ilist, where, reg_ptr,
-                                  DR_REG_NULL, sizeof(mem_ref_t));
+                                  DR_REG_NULL, sizeof(ins_ref_t));
+
     /* Restore scratch registers */
     if (drreg_unreserve_register(drcontext, ilist, where, reg_ptr) != DRREG_SUCCESS ||
         drreg_unreserve_register(drcontext, ilist, where, reg_tmp) != DRREG_SUCCESS)
         DR_ASSERT(false);
 }
 
-/* insert inline code to add a memory reference info entry into the buffer */
-static void
-instrument_mem(void *drcontext, instrlist_t *ilist, instr_t *where,
-               opnd_t ref, bool write)
-{
-    /* We need two scratch registers */
-    reg_id_t reg_ptr, reg_tmp;
-    if (drreg_reserve_register(drcontext, ilist, where, NULL, &reg_ptr) !=
-        DRREG_SUCCESS ||
-        drreg_reserve_register(drcontext, ilist, where, NULL, &reg_tmp) !=
-        DRREG_SUCCESS) {
-        DR_ASSERT(false); /* cannot recover */
-        return;
-    }
-    /* save_addr should be called first as reg_ptr or reg_tmp maybe used in ref */
-    insert_save_addr(drcontext, ilist, where, ref, reg_ptr, reg_tmp);
-    insert_save_type(drcontext, ilist, where, reg_ptr, reg_tmp,
-                     write ? REF_TYPE_WRITE : REF_TYPE_READ);
-    insert_save_size(drcontext, ilist, where, reg_ptr, reg_tmp,
-                     (ushort)drutil_opnd_mem_size_in_bytes(ref, where));
-    instrlist_set_auto_predicate(ilist, instr_get_predicate(where));
-    drx_buf_insert_update_buf_ptr(drcontext, trace_buffer, ilist, where, reg_ptr,
-                                  DR_REG_NULL, sizeof(mem_ref_t));
-    instrlist_set_auto_predicate(ilist, DR_PRED_NONE);
-    /* Restore scratch registers */
-    if (drreg_unreserve_register(drcontext, ilist, where, reg_ptr) != DRREG_SUCCESS ||
-        drreg_unreserve_register(drcontext, ilist, where, reg_tmp) != DRREG_SUCCESS)
-        DR_ASSERT(false);
-}
-
-/* For each memory reference app instr, we insert inline code to fill the buffer
- * with an instruction entry and memory reference entries.
- */
+/* For each app instr, we insert inline code to fill the buffer. */
 static dr_emit_flags_t
 event_app_instruction(void *drcontext, void *tag, instrlist_t *bb,
                       instr_t *instr, bool for_trace,
                       bool translating, void *user_data)
 {
-    int i;
+    /* we don't want to auto-predicate any instrumentation */
+    drmgr_disable_auto_predication(drcontext, bb);
 
     if (!instr_is_app(instr))
         return DR_EMIT_DEFAULT;
-    if (!instr_reads_memory(instr) && !instr_writes_memory(instr))
-        return DR_EMIT_DEFAULT;
 
-    /* insert code to add an entry for app instruction */
-    instrlist_set_auto_predicate(bb, DR_PRED_NONE);
+    /* insert code to add an entry to the buffer */
     instrument_instr(drcontext, bb, instr);
-    instrlist_set_auto_predicate(bb, instr_get_predicate(instr));
 
-    /* insert code to add an entry for each memory reference opnd */
-    for (i = 0; i < instr_num_srcs(instr); i++) {
-        if (opnd_is_memory_reference(instr_get_src(instr, i)))
-            instrument_mem(drcontext, bb, instr, instr_get_src(instr, i), false);
-    }
-
-    for (i = 0; i < instr_num_dsts(instr); i++) {
-        if (opnd_is_memory_reference(instr_get_dst(instr, i)))
-            instrument_mem(drcontext, bb, instr, instr_get_dst(instr, i), true);
-    }
-
-    return DR_EMIT_DEFAULT;
-}
-
-/* We transform string loops into regular loops so we can more easily
- * monitor every memory reference they make.
- */
-static dr_emit_flags_t
-event_bb_app2app(void *drcontext, void *tag, instrlist_t *bb,
-                 bool for_trace, bool translating)
-{
-    if (!drutil_expand_rep_string(drcontext, bb)) {
-        DR_ASSERT(false);
-        /* in release build, carry on: we'll just miss per-iter refs */
-    }
     return DR_EMIT_DEFAULT;
 }
 
@@ -247,14 +162,13 @@ event_thread_init(void *drcontext)
      * in a path as a client argument.
      */
     data->log = log_file_open(client_id, drcontext, NULL /* using client lib path */,
-                              "memtrace",
+                              "instrace",
 #ifndef WINDOWS
                               DR_FILE_CLOSE_ON_FORK |
 #endif
                               DR_FILE_ALLOW_LARGE);
     data->logf = log_stream_from_file(data->log);
-    fprintf(data->logf,
-            "Format: <data address>: <data size>, <(r)ead/(w)rite/opcode>\n");
+    fprintf(data->logf, "Format: <instr address>,<opcode>\n");
 }
 
 static void
@@ -271,12 +185,10 @@ event_exit(void)
     if (!drmgr_unregister_tls_field(tls_idx) ||
         !drmgr_unregister_thread_init_event(event_thread_init) ||
         !drmgr_unregister_thread_exit_event(event_thread_exit) ||
-        !drmgr_unregister_bb_app2app_event(event_bb_app2app) ||
         !drmgr_unregister_bb_insertion_event(event_app_instruction) ||
         drreg_exit() != DRREG_SUCCESS)
         DR_ASSERT(false);
 
-    drutil_exit();
     drmgr_exit();
 
     drx_buf_free(trace_buffer);
@@ -288,17 +200,15 @@ dr_client_main(client_id_t id, int argc, const char *argv[])
 {
     /* We need 2 reg slots beyond drreg's eflags slots => 3 slots */
     drreg_options_t ops = {sizeof(ops), 3, false};
-    dr_set_client_name("DynamoRIO Sample Client 'memtrace'",
+    dr_set_client_name("DynamoRIO Sample Client 'instrace'",
                        "http://dynamorio.org/issues");
-    if (!drmgr_init() || drreg_init(&ops) != DRREG_SUCCESS || !drutil_init() ||
-        !drx_init())
+    if (!drmgr_init() || drreg_init(&ops) != DRREG_SUCCESS || !drx_init())
         DR_ASSERT(false);
 
     /* register events */
     dr_register_exit_event(event_exit);
     if (!drmgr_register_thread_init_event(event_thread_init) ||
         !drmgr_register_thread_exit_event(event_thread_exit) ||
-        !drmgr_register_bb_app2app_event(event_bb_app2app, NULL) ||
         !drmgr_register_bb_instrumentation_event(NULL /*analysis_func*/,
                                                  event_app_instruction,
                                                  NULL))
@@ -309,9 +219,9 @@ dr_client_main(client_id_t id, int argc, const char *argv[])
     tls_idx = drmgr_register_tls_field();
     DR_ASSERT(tls_idx != -1);
 
-    trace_buffer = drx_buf_create_trace_buffer(MEM_BUF_SIZE, memtrace);
+    trace_buffer = drx_buf_create_trace_buffer(MEM_BUF_SIZE, instrace);
     DR_ASSERT(trace_buffer != NULL);
 
     /* make it easy to tell, by looking at log file, which client executed */
-    dr_log(NULL, LOG_ALL, 1, "Client 'memtrace' initializing\n");
+    dr_log(NULL, LOG_ALL, 1, "Client 'instrace' initializing\n");
 }
